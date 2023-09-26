44ad53e6239c5b64bf787f7bbb754ff1018fb159
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-09-26 14:00:25 +0300

    Classes are done

---
+++ src/com/workintech/compositionProject/Wall.java
@@ -0,0 +1,18 @@
+package com.workintech.compositionProject;
+
+public class Wall {
+    private String direction;
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+
+    public void create() {
+        System.out.println("The direction is: " + getDirection());
+    }
+}
