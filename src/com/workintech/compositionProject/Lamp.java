8874714da8f80ca3c70f5ebadd1616bc4400cac8
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-09-26 13:32:34 +0300

    Lamp class done

---
+++ src/com/workintech/compositionProject/Lamp.java
@@ -0,0 +1,50 @@
+package com.workintech.compositionProject;
+
+public class Lamp {
+    private LampType lampType;
+    private boolean battery;
+    private int globRating;
+
+    public Lamp(LampType lampType, boolean battery, int globRating) {
+        this.lampType = lampType;
+        this.battery = battery;
+        this.globRating = globRating;
+    }
+
+    public LampType getLampType() {
+        return lampType;
+    }
+
+    public void setLampType(LampType lampType) {
+        this.lampType = lampType;
+    }
+
+    public boolean isBattery() {
+        return battery;
+    }
+
+    public void setBattery(boolean battery) {
+        this.battery = battery;
+    }
+
+    public int getGlobRating() {
+        return globRating;
+    }
+
+    public void setGlobRating(int globRating) {
+        this.globRating = globRating;
+    }
+
+    public static void turnOn() {
+        System.out.println("Lamp is being turned on.");
+    }
+    public LampType getStyle(LampType lampType){
+        return this.lampType;
+    }
+
+    public int getGlobalRating(){
+        return globRating;
+    }
+
+
+}
---
+++ src/com/workintech/compositionProject/LampType.java
@@ -0,0 +1,8 @@
+package com.workintech.compositionProject;
+
+public enum LampType {
+    LED,
+    FLORESAN,
+    ACOR;
+
+}
